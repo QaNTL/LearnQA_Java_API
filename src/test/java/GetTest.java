a0f12e80c5d89f2d32e1725fd37d036c385daacb
NNNN <qamail@rambler.ru>
2024-02-17 10:35:36 +0300

    GetTestAll

--- src/test/java/GetTest.java
+++ src/test/java/GetTest.java
@@ -1,1 +1,14 @@
+import io.restassured.response.Response;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
 
+public class GetTest {
+
+    @Test
+    public void testAPI(){
+        Response response=RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
